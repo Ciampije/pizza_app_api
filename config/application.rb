3ec0ccb8c08072b3320d6c6ae01ce538c86e9726
Ciampije <josephciampi@C1MRQ06AHF4H.fios-router.home>
2017-05-31 01:27:49 -0400

    cors

--- config/application.rb
+++ config/application.rb
@@ -28,11 +28,6 @@ module PizzaAppApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     require "dotenv-rails"
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
+
   end
 end
