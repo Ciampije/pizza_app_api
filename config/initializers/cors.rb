39de8e8eff95a1a891c49cc7507964e1b314e4f4
Ciampije <josephciampi@C1MRQ06AHF4H.fios-router.home>
2017-05-31 01:45:08 -0400

    herokuagain

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,17 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
+    origins '*'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :update, :delete, :options, :head]
+  end
+  allow do
     origins 'localhost:3000', 'https://pizzaappapi.herokuapp.com'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :update, :delete, :options, :head]
   end
 end
