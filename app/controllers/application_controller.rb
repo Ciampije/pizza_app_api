a59832198b180497366305ef9ea8a4855d5806b0
Ciampije <josephciampi@C1MRQ06AHF4H.fios-router.home>
2017-05-31 13:34:32 -0400

    kson json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,7 @@ class ApplicationController < ActionController::API
       puts "AUTHORIZE USER"
       puts "user id: #{get_current_user.id}"
       puts "params: #{params[:id]}"
-      render kson: {status: 401, message: 'Unauthorized'} unless get_current_user.id == params[:id].to_i
+      render json: {status: 401, message: 'Unauthorized'} unless get_current_user.id == params[:id].to_i
   end
 
 end
